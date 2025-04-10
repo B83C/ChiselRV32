1b6b99bc8df72b705bd9dd651ade5f6e0e0454a2
Razhely <534355015@qq.com>
2025-04-10 23:29:21 +0800

    新增LSU核心接口，扩展信号以支持地址生成、执行单元响应和性能监控

--- src/main/rsd_rv32/execution/lsu.scala
+++ src/main/rsd_rv32/execution/lsu.scala
@@ -45,3 +45,77 @@ class LSUMemIO(implicit p: Parameters , edge: TLEdgeOut) extends Bundle()(p) {
   })
   //指示信号的获取和释放，进行性能的监控
 }
+
+class LSUCoreIO(implicit p: Parameters) extends Bundle()(p) {
+  //这里的MenGen定义为
+  //class MemGen(implicit p: Parameters) extends BoomBundle
+  // with HasBoomUOP
+  //{
+  //val data = UInt(xLen.W)
+  //}
+    val agen = Flipped(Vec(p.lsuWidth, Valid(new MemGen)))//地址生成器的输入
+    val dgen = Flipped(Vec(p.memWidth + 1, Valid(new MemGen)))//数据生成器的输入,memWidth为内存宽度
+  //class Wakeup(implicit p: Parameters) extends BoomBundle
+  //with HasBoomUOP
+  //{
+  //val bypassable = Bool()
+  //val speculative_mask = UInt(aluWidth.W)
+  //val rebusy = Bool()
+  //}
+    val iwakeups = Vec(p.lsuWidth, Valid(new Wakeup))//LSU的唤醒信号
+    val iresp = Vec(p.lsuWidth, Valid(new ExeUnitResp(xLen)))
+    val fresp  = Vec(p.lsuWidth, Valid(new ExeUnitResp(xLen)))
+    //执行单元返回的响应数据
+    val sfence = Flipped(Valid(new rocket.SFenceReq))//传递sfence的请求，用于内容屏蔽
+
+    val dis_uops = Vec(p.coreWidth, Valid(new MicroOp))//解码后的微操作
+    //coreWidth表示时钟周期的指令数量
+
+    val dis_ldq_idx = Output(Vec(p.coreWidth,UInt((1+ldpAddrSz).W)))
+    val dis_stq_idx = Output(Vec(p.coreWidth,UInt((1+stqAddrSz).W)))
+    //LDQ和STQ索引,用于跟踪指令的队列位置
+
+    val ldq_full = Output(Vec(p.coreWidth,Bool()))
+    val stq_full = Output(Vec(p.coreWidth,Bool()))
+    //表示LDQ和STQ是否满了的信号 
+    
+    val commit = Input(new CommitInfo)//提交信号 CommitInfo是一个Bundle，包含了提交的指令信息
+    val commitLoadAtRobHead = Input(Bool())//当前是否在rob的头部进行加载
+
+    val clr_busy_bit = Output(Bool())//清除busybit的信号
+    val clr_unsafe = Output(Bool())//清除不安全的加载状态
+
+    val fence_dmem = Input(Bool())//控制内存屏障操作
+    val bradate = Input(new bradateInfo)//分支更新信息
+
+    val rob_head_idx = Input(UInt((p.robAddrSz).W))//rob的头部索引
+    val rob_pnr_idx = Input(UInt((p.robAddrSz).W))//rob的后备索引
+    
+    val exception = Input(Bool())//异常信号
+
+    val Fenlei_ready = Output(Bool())//表示FENCEI是否准备好
+    val load_excp = Output(Valid(new Exception))//表示加载异常的信号
+
+    val tsc_reg = Input(UInt())//时间戳寄存器(计数器)的值
+
+    val status = Input(new rocket.MStatus)//接收机器模式状态寄存器的值
+
+    val bp = Input(Vec(breakpoint_num,new rocket.BP))//接收断点设置
+    val mach_context = Input(UInt)//接收机器模式上下文的值
+    val scontext = Input(UInt)//接收超线程模式上下文的值
+
+    val perf =Output(new Bundle {
+      val acquire = Bool()
+      val release = Bool()
+      val tlbMiss = Bool()//tlb可能不需要，此处待定
+    })
+    //输出信号，用于性能监控
+}
+
+//TLE部分可能不需要做，待定
+
+class LSUIO(implicit p: Parameters,edge : TLEdgeOut) extends Bundle()(p){
+  val ptw = new rocket.TLBPTWIO//TLB的输入输出接口,待定
+  val core = new LSUCoreIO//LSU的核心接口
+  val dcache_mem = new LSUMemIO//LSU的内存接口
+}
