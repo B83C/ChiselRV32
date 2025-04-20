3999c330b245ad89340dffcbc44d60014e913308
HYH-ZT <23307140040@m.fudan.edu.cn>
2025-04-20 16:59:38 +0800

    Refactor ROBIO interface: remove commented-out Dispatch_ROB_Interface and reorganize signals for clarity

--- src/main/scala/rsd_rv32/scheduler/rob.scala
+++ src/main/scala/rsd_rv32/scheduler/rob.scala
@@ -5,7 +5,7 @@ import rsd_rv32.common._
 
 
 
-
+/*
 class Dispatch_ROB_Interface(implicit p: Parameters) extends Bundle {
     val dis_uops = Valid(Vec(p.DISPATCH_WIDTH, new uop()))  //Dispatch Unit的uop
 
@@ -24,12 +24,21 @@ class WB_ROB_Interface(implicit p: Parameters) extends Bundle {
 class ROB_broadcast(implicit p: Parameters) extends Bundle {
     val commit_signal = Valid(Vec(p.DISPATCH_WIDTH, UInt((37 + ((34 + p.GHR_WIDTH) max (37 + log2Ceil(p.PRF_DEPTH)))).W))) //ROB条目
 }
-
+*/
 
 class ROBIO(implicit p: Parameters) extends Bundle {
-    val dis_rob = Flipped(new Dispatch_ROB_Interface())
-    val exu_rob = Flipped(new EXU_ROB_Interface())
-    val rob_commit = new ROB_broadcast()
+    val dis_uops = Valid(Vec(p.DISPATCH_WIDTH, new uop()))  //Dispatch Unit的uop
+
+    val rob_empty = Input(Bool())  //ROB空标志(0表示空，1表示非空)
+    val rob_head = Input(UInt(log2Ceil(p.ROB_DEPTH))) //ROB头指针
+    val rob_tail = Input(UInt(log2Ceil(p.ROB_DEPTH).W)) //ROB尾指针
+
+    val complete_map = Input(Vec(p.FU_NUM, Bool()))  //完成映射表
+    val complete_uop = Input(Vec(p.FU_NUM, new uop()))  //来自exu的uop
+    val mispred = Input(Bool()) //分支误预测信号
+    val if_jump = Input(Bool()) //分支指令跳转信号
+
+    val commit_signal = Valid(Vec(p.DISPATCH_WIDTH, UInt((37 + ((34 + p.GHR_WIDTH) max (37 + log2Ceil(p.PRF_DEPTH)))).W))) //ROB条目
 }
 // 重命名缓冲区，主要用于存储指令的执行结果。它是一个FIFO结构，先进先出。指令在执行完成后，将结果写入ROB中。ROB中的数据可以被其他指令读取，从而实现数据的共享和重用。
 class ROB(implicit p: Parameters) extends Module {
