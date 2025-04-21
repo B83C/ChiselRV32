cc1bd9784eca75d99f874f73d07dc12eb823d173
fejer-kernel <1928899380@qq.com>
2025-04-21 12:41:30 +0800

    fix dispathcer interface

--- src/main/scala/rsd_rv32/scheduler/dispatcher.scala
+++ src/main/scala/rsd_rv32/scheduler/dispatcher.scala
@@ -14,10 +14,13 @@ class Dispatcher_IO(implicit p:Parameters) extends Bundle {
   val rob_tail = Input(UInt(log2Ceil(p.ROB_DEPTH).W))
   // with exu_issue
   val exuissue_uop = Valid(Vec(p.DISPATCH_WIDTH,new DISPATCH_EXUISSUE_uop()))
+  val exuissued_id = Valid(Vec(p.EXU_ISSUE_WIDTH,UInt(log2Ceil(p.EXUISSUE_DEPTH).W))).flip
   // with st_issue
   val stissue_uop = Valid(Vec(p.DISPATCH_WIDTH,new DISPATCH_STISSUE_uop()))
+  val stissued_id = Valid(UInt(log2Ceil(p.STISSUE_DEPTH).W)).flip
   // with ld_issue
   val ldissue_uop = Valid(Vec(p.DISPATCH_WIDTH,new DISPATCH_LDISSUE_uop()))
+  val ldissued_id = Valid(UInt(log2Ceil(p.LDISSUE_DEPTH).W)).flip
   // with sq
   val stq_empty = Input(Bool())
   val stq_head = Input(UInt(log2Ceil(p.STQ_DEPTH).W))
