ccb2ab138d3cfb5debb7b969b9290e3f2675f809
fejer-kernel <1928899380@qq.com>
2025-04-23 22:19:54 +0800

    fix

--- src/main/scala/rsd_rv32/common/common.scala
+++ src/main/scala/rsd_rv32/common/common.scala
@@ -14,14 +14,19 @@ case class Parameters(
   // DISPATCH_WIDTH: Int = 2,
   // ISSUE_WIDTH: Int = 2,
   GHR_WIDTH: Int = 8,
+
   FU_NUM: Int = 7,
   ALU_NUM: Int = 4,
   BU_NUM: Int = 1,
   STU_NUM: Int = 1,
   LDU_NUM: Int = 1,
+
   PRF_DEPTH: Int = 128,
+
   ROB_DEPTH: Int = 256,
+
   STQ_DEPTH: Int = 32,
+  
   EXUISSUE_DEPTH: Int = 16,
   LDISSUE_DEPTH: Int = 16,
   STISSUE_DEPTH: Int = 16
