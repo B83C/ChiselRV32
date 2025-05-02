714f82abad6fbcac0b9ce785b7dbdbea29f0db64
HYH-ZT <23307140040@m.fudan.edu.cn>
2025-05-02 17:45:24 +0800

    Refactor exu_issue module: update wb_uop signals and enhance issue queue logic with ready signal handling for ps1 and ps2

--- src/main/scala/rsd_rv32/scheduler/exu_issue.scala
+++ src/main/scala/rsd_rv32/scheduler/exu_issue.scala
@@ -28,9 +28,9 @@ class exu_issue_IO(implicit p: Parameters) extends Bundle {
     //监听PRF的valid信号用于更新ready状态
     val prf_valid = Input(Vec(p.PRF_DEPTH, Bool())) //PRF的valid信号
     //监听FU后级间寄存器内的物理寄存器ready信号
-    val wb_uop2 = Flipped(Valid((Vec(p.FU_NUM - p.BU_NUM - p.STU_NUM, new ALU_WB_uop()))))  //来自alu、mul、div、load pipeline的uop
+    val wb_uop2 = Flipped((Vec(p.FU_NUM - p.BU_NUM - p.STU_NUM, Valid(new ALU_WB_uop()))))  //来自alu、mul、div、load pipeline的uop
     //监听FU处物理寄存器的ready信号
-    val wb_uop1 = Flipped(Valid((Vec(p.FU_NUM - p.BU_NUM - p.STU_NUM, new ALU_WB_uop())))) //来自alu、mul、div、load pipeline的uop
+    val wb_uop1 = Flipped((Vec(p.FU_NUM - p.BU_NUM - p.STU_NUM, Valid(new ALU_WB_uop())))) //来自alu、mul、div、load pipeline的uop
     //val ldu_wb_uop1 = Flipped(Valid(new LDPIPE_WB_uop()))  //来自ldu的uop
 
     //输出至Dispatch Unit的信号
@@ -40,6 +40,74 @@ class exu_issue_IO(implicit p: Parameters) extends Bundle {
     val rob_commitsignal = Vec(p.CORE_WIDTH, Flipped(Valid(new ROBContent()))) //ROB提交时的广播信号，发生误预测时对本模块进行冲刷
 }
 
+class exu_issue_content(implicit p: Parameters) extends Bundle {
+    val busy = Bool() //busy flag
+    val instr_type = InstrType() //指令类型
+    val ps1 = UInt(log2Ceil(p.PRF_DEPTH)) //操作数1的物理寄存器地址
+    val ready1 = Bool() //操作数1的ready信号
+    val ps2 = UInt(log2Ceil(p.PRF_DEPTH)) //操作数2的物理寄存器地址
+    val ready2 = Bool() //操作数2的ready信号
+}
+
 class exu_issue_queue(implicit p: Parameters) extends Module {
     val io = IO(new exu_issue_IO())
+    val issue_queue = RegInit(Seq.fill(p.EXUISSUE_DEPTH)(0.U.asTypeOf(new exu_issue_content()))) //发射队列
+    val payload = RegInit(Seq.fill(p.EXUISSUE_DEPTH)(0.U.asTypeOf(new DISPATCH_EXUISSUE_uop()))) //发射队列的有效载荷
+
+    //来自Dispatch的入队指令
+    when(io.dis_uop(0).valid && !io.dis_uop(1).valid){
+        payload(io.dis_uop(0).iq_id) := io.dis_uop(0)
+        issue_queue(io.dis_uop(0).iq_id).busy := true.B
+        issue_queue(io.dis_uop(0).iq_id).instr_type := io.dis_uop(0).instr_type
+        issue_queue(io.dis_uop(0).iq_id).ps1 := io.dis_uop(0).ps1
+        issue_queue(io.dis_uop(0).iq_id).ps2 := io.dis_uop(0).ps2
+
+        //入队时判断ps1和ps2的ready信号
+        val conditions1 = for (i <- 0 until (p.FU_NUM - p.BU_NUM - p.STU_NUM)) yield {
+            io.prf_valid(io.dis_uop(0).ps1) ||
+            (io.wb_uop1(i).valid && io.wb_uop1(i).bits.pdst === io.dis_uop(0).ps1) ||
+            (io.wb_uop2(i).valid && io.wb_uop2(i).bits.pdst === io.dis_uop(0).ps1)
+        }
+        val conditions2 = for (i <- 0 until (p.FU_NUM - p.BU_NUM - p.STU_NUM)) yield {
+            io.prf_valid(io.dis_uop(0).ps2) ||
+            (io.wb_uop1(i).valid && io.wb_uop1(i).bits.pdst === io.dis_uop(0).ps2) ||
+            (io.wb_uop2(i).valid && io.wb_uop2(i).bits.pdst === io.dis_uop(0).ps2)
+        }
+        when (conditions1.reduce(_ || _)) {
+            issue_queue(io.dis_uop(0).iq_id).ready1 := true.B
+        }
+        when (conditions2.reduce(_ || _)) {
+            issue_queue(io.dis_uop(0).iq_id).ready2 := true.B
+        }
+        //结束ready信号赋值
+    } .elsewhen(io.dis_uop(0).valid && io.dis_uop(1).valid){
+        for (k <- 0 until 2){
+            payload(io.dis_uop(k).iq_id) := io.dis_uop(k)
+            issue_queue(io.dis_uop(k).iq_id).busy := true.B
+            issue_queue(io.dis_uop(k).iq_id).instr_type := io.dis_uop(k).instr_type
+            issue_queue(io.dis_uop(k).iq_id).ps1 := io.dis_uop(k).ps1
+            issue_queue(io.dis_uop(k).iq_id).ps2 := io.dis_uop(k).ps2
+
+            //入队时判断ps1和ps2的ready信号
+            val conditions1 = for (i <- 0 until (p.FU_NUM - p.BU_NUM - p.STU_NUM)) yield {
+                io.prf_valid(io.dis_uop(k).ps1) ||
+                (io.wb_uop1(i).valid && io.wb_uop1(i).bits.pdst === io.dis_uop(k).ps1) ||
+                (io.wb_uop2(i).valid && io.wb_uop2(i).bits.pdst === io.dis_uop(k).ps1)
+            }
+            val conditions2 = for (i <- 0 until (p.FU_NUM - p.BU_NUM - p.STU_NUM)) yield {
+                io.prf_valid(io.dis_uop(k).ps2) ||
+                (io.wb_uop1(i).valid && io.wb_uop1(i).bits.pdst === io.dis_uop(k).ps2) ||
+                (io.wb_uop2(i).valid && io.wb_uop2(i).bits.pdst === io.dis_uop(k).ps2)
+            }
+            when (conditions1.reduce(_ || _)) {
+                issue_queue(io.dis_uop(k).iq_id).ready1 := true.B
+            }
+            when (conditions2.reduce(_ || _)) {
+                issue_queue(io.dis_uop(k).iq_id).ready2 := true.B
+            }
+            //结束ready信号赋值
+        }
+    }.otherwise{
+        //不入队
+    }
 }
