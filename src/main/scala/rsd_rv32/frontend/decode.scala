7da51fb5ec8f936e7cd15236ae4935ecb6c22e7e
EasonWind <xingyc6@163.com>
2025-05-05 17:01:20 +0800

    Update decode.scala

--- src/main/scala/rsd_rv32/frontend/decode.scala
+++ src/main/scala/rsd_rv32/frontend/decode.scala
@@ -15,6 +15,34 @@ class Decode_IO(implicit p: Parameters) extends Bundle {
   val rob_commitsignal = Vec(p.CORE_WIDTH, Flipped(Valid(new ROBContent()))) // ROB提交时的广播信号，发生误预测时对本模块进行冲刷
 }
 
+
+//ID到RENAME的uop定义
+class ID_RENAME_uop(implicit p:Parameters) extends Bundle{
+  val pc = 
+
+
+}
+
+
+
+
 class DecodeUnit(implicit p: Parameters) extends Module {
   val io = IO(new Decode_IO())
+  
+  //是否接受指令
+  val rob_flush = io.rob_commitsignal.map(_.valid)reduce(_||_)  //是否要flush
+  io.id_ready = io.rename_ready && !rob_flush
+
+  for (i <-0 until p.CORE_WIDTH{
+    val if_uop = io.if_uop(i)
+    val rename_uop = Wire(Valid(new ID_RENAME_uop()))
+    
+    
+    )
+
+  
+
+
+
+  
 }
