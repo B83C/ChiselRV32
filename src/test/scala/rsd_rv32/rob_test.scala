f2a60b0b67b737c756d5f4d59e55e7f46483411c
HYH-ZT <23307140040@m.fudan.edu.cn>
2025-05-10 20:25:40 +0800

    完善rob_test

--- src/test/scala/rsd_rv32/rob_test.scala
+++ src/test/scala/rsd_rv32/rob_test.scala
@@ -52,7 +52,6 @@ class rob_test extends AnyFlatSpec with ChiselScalatestTester {
       c.io.dis_uop(1).bits.instr_type.poke(InstrType.Branch)
       c.io.dis_uop(1).bits.btb_hit.poke(BTBHit.H)
       c.io.dis_uop(1).bits.GHR.poke(2.U)
-      c.io.dis_uop(1).bits.branch_pred.poke(BranchPred.T)
       c.clock.step()
       c.io.dis_uop(0).valid.poke(1.U)
       c.io.dis_uop(1).valid.poke(0.U)
@@ -65,7 +64,7 @@ class rob_test extends AnyFlatSpec with ChiselScalatestTester {
       println("pass!")
     }
   }
-  "rob" should "update correctly" in {
+  "rob" should "update, retire and flush correctly" in {
     test(new ROB()) { c =>
       c.io.dis_uop(0).valid.poke(1.U)
       c.io.dis_uop(0).bits.instr_addr.poke("h0000ffff".U)
@@ -78,23 +77,25 @@ class rob_test extends AnyFlatSpec with ChiselScalatestTester {
       c.io.dis_uop(1).bits.instr_type.poke(InstrType.Branch)
       c.io.dis_uop(1).bits.btb_hit.poke(BTBHit.H)
       c.io.dis_uop(1).bits.GHR.poke(2.U)
-      c.io.dis_uop(1).bits.branch_pred.poke(BranchPred.T)
       c.clock.step()
       c.io.dis_uop(0).valid.poke(0.U)
       c.io.dis_uop(1).valid.poke(0.U)
       c.io.alu_wb_uop(1).valid.poke(1.U)
       c.io.alu_wb_uop(1).bits.rob_index.poke(0.U)
 
+      c.clock.step()
       c.io.bu_wb_uop(0).valid.poke(1.U)
       c.io.bu_wb_uop(0).bits.rob_index.poke(1.U)
       c.io.bu_wb_uop(0).bits.mispred.poke(1.U)
       c.io.bu_wb_uop(0).bits.target_PC.poke("h0000aaaa".U)
       c.io.bu_wb_uop(0).bits.is_conditional.poke(1.U)
       c.io.bu_wb_uop(0).bits.branch_direction.poke(BranchPred.T)
-      c.clock.step()
+      c.io.alu_wb_uop(0).valid.poke(0.U)
       c.io.rob_commitsignal(0).valid.expect(1.U)
-      c.io.rob_commitsignal(1).valid.expect(1.U)
-      c.clock.step(2)
+      c.io.rob_commitsignal(1).valid.expect(0.U)
+      c.clock.step()
+      c.io.bu_wb_uop(0).valid.poke(0.U)
+      c.clock.step(3)
       println("pass!")
     }
   }
@@ -120,6 +121,30 @@ class rob_test extends AnyFlatSpec with ChiselScalatestTester {
       println("pass!")
     }
   }
-
+  "rob" should "head and tail move correctly" in {
+    test(new ROB()) { c =>
+      c.io.dis_uop(0).valid.poke(1.U)
+      c.io.dis_uop(0).bits.instr_type.poke(InstrType.ALU)
+      c.io.dis_uop(0).bits.instr_addr.poke(1.U)
+      c.io.dis_uop(1).valid.poke(1.U)
+      c.io.dis_uop(1).bits.instr_type.poke(InstrType.ALU)
+      c.io.dis_uop(1).bits.instr_addr.poke(1.U)
+      c.clock.step(5)
+      c.io.alu_wb_uop(0).valid.poke(1.U)
+      c.io.alu_wb_uop(0).bits.rob_index.poke(0.U)
+      c.io.alu_wb_uop(1).valid.poke(1.U)
+      c.io.alu_wb_uop(1).bits.rob_index.poke(3.U)
+      c.clock.step()
+      println("after WB")
+      c.io.alu_wb_uop(0).valid.poke(0.U)
+      c.io.alu_wb_uop(0).valid.poke(0.U)
+      c.clock.step(2)
+      c.io.dis_uop(1).valid.poke(0.U)
+      c.clock.step()
+      c.io.dis_uop(0).valid.poke(0.U)
+      c.clock.step(2)
+      println("pass!")
+    }
+  }
 }
 
