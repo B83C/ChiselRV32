65d640f7a603c93f9ccd92ac9d0e4f4c4b6a4d0f
HYH-ZT <23307140040@m.fudan.edu.cn>
2025-05-16 23:28:13 +0800

    small fix

--- src/main/scala/rsd_rv32/execution/execution.scala
+++ src/main/scala/rsd_rv32/execution/execution.scala
@@ -16,8 +16,9 @@ class EXUIO(fu_num: UInt)(implicit p: Parameters) extends Bundle{
   // val wb = Output(Vec(p.FU_NUM, Valid(new WB_uop())))
   
   // val branch_info = Output(new FUBranchInfo())
-  // val rob_signal = Input(new ROBSignal())
-  
+  //val rob_signal = Input(new ROBSignal())
+  val rob_commitsignal = Input(Vec(p.CORE_WIDTH, Valid(new ROBContent())))
+  //flush = rob_commitsignal(0).valid && rob_commitsignal(0).bits.mispred
   // Should be removed in place of more general readys
   //反馈给exu_issue queue的信号
   val mul_ready = Output(Bool())
