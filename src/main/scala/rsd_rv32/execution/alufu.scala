836cd84e378827ff367d46c1c5f73e0d460a3089
mjy0007 <1638293265@qq.com>
2025-05-20 00:35:47 +0800

    Update alufu.scala

--- src/main/scala/rsd_rv32/execution/alufu.scala
+++ src/main/scala/rsd_rv32/execution/alufu.scala
@@ -10,7 +10,7 @@ import Utils._
 class ALUFU(implicit p: Parameters) extends FunctionalUnit() with ALUConsts  {
   override def supportedInstrTypes = Set(InstrType.ALU)
   // 为了配合上一级的uop，输出按FU区分
-  val out = IO(Valid(new ALU_WB_uop()))
+  val out = Valid(new ALU_WB_uop())
   
   val internal_alu = Module(new ALU())
   val fu_signals = io.uop.bits.fu_signals
@@ -30,36 +30,39 @@ class ALUFU(implicit p: Parameters) extends FunctionalUnit() with ALUConsts  {
   internal_alu.io.in1 := Sel(fu_signals.opr1_sel, io.uop.bits.ps1_value)
   internal_alu.io.in2 := Sel(fu_signals.opr2_sel, io.uop.bits.ps2_value)
 
-  internal_alu.io.fn := MuxLookup(io.uop.bits.instr(6, 0), ALU_ADD)(Seq(
+
+  internal_alu.io.fn := Mux(is_AUIPC, ALU_ADD,MuxLookup(fu_signals.opr2_sel, ALU_ADD)(Seq(
     // 立即数指令 (I-type)
-    "b0010011".U -> MuxLookup(io.uop.bits.instr(14, 12), ALU_ADD)(Seq(
+    OprSel.IMM -> MuxLookup(io.uop.bits.instr(7, 5), ALU_ADD)(Seq(
       "b000".U -> ALU_ADD,  // ADDI
       "b001".U -> ALU_SLL,  // SLLI
       "b010".U -> ALU_SLT,  // SLTI
       "b011".U -> ALU_SLTU, // SLTIU
       "b100".U -> ALU_XOR,  // XORI
-      "b101".U -> Mux(io.uop.bits.instr(30), ALU_SRA, ALU_SRL), // SRAI/SRLI
+      "b101".U -> Mux(io.uop.bits.instr(23), ALU_SRA, ALU_SRL), // SRAI/SRLI
       "b110".U -> ALU_OR,   // ORI
       "b111".U -> ALU_AND   // ANDI
     )),
     // 寄存器指令 (R-type)
-    "b0110011".U -> MuxLookup(io.uop.bits.instr(14, 12), ALU_ADD)(Seq(
-      "b000".U -> Mux(io.uop.bits.instr(30), ALU_SUB, ALU_ADD), // SUB/ADD
+    OprSel.REG -> MuxLookup(io.uop.bits.instr(7, 5), ALU_ADD)(Seq(
+      "b000".U -> Mux(io.uop.bits.instr(23), ALU_SUB, ALU_ADD), // SUB/ADD
       "b001".U -> ALU_SLL,  // SLL
       "b010".U -> ALU_SLT,  // SLT
       "b011".U -> ALU_SLTU, // SLTU
       "b100".U -> ALU_XOR,  // XOR
-      "b101".U -> Mux(io.uop.bits.instr(30), ALU_SRA, ALU_SRL), // SRA/SRL
+      "b101".U -> Mux(io.uop.bits.instr(23), ALU_SRA, ALU_SRL), // SRA/SRL
       "b110".U -> ALU_OR,   // OR
       "b111".U -> ALU_AND   // AND
     )),
   ))
+  )
 
 
   val data_out = Wire(new ALU_WB_uop())
   data_out.pdst := io.uop.bits.pdst
   data_out.pdst_value := internal_alu.io.out
   data_out.rob_index := io.uop.bits.rob_index
+  data_out.instr := io.uop.bits.instr
 
   out.valid := io.uop.valid
   out.bits := data_out
@@ -75,7 +78,6 @@ class ALUIO(implicit p: Parameters) extends Bundle {
 
   // 输出结果
   val out = Output(UInt(p.XLEN.W))
-  val cmp_out = Output(Bool())  // 比较结果
 }
 
 class ALU(implicit p: Parameters) extends Module with ALUConsts {
