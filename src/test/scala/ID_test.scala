529cc9d08ed30644ed1cdea108e889634bd3a8f5
EasonWind <xingyc6@163.com>
2025-05-19 16:58:35 +0800

    Update ID_test.scala

--- src/test/scala/ID_test.scala
+++ src/test/scala/ID_test.scala
@@ -2,7 +2,7 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import rsd_rv32.common._
-import rsd_rv32.frontend.BranchPredictorUnit
+import rsd_rv32.frontend.decode
 
 class DecodeUnitTest extends AnyFlatSpec with ChiselScalatestTester {
   
