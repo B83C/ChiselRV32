fadc7f7dd5ef8ab35d2e6aa1cce92e256ea93f31
EasonWind <xingyc6@163.com>
2025-05-20 16:02:42 +0800

    Update IF_test.scala

--- src/test/scala/IF_test.scala
+++ src/test/scala/IF_test.scala
@@ -5,8 +5,8 @@ import rsd_rv32.frontend._
 import rsd_rv32.common._
 
 class FetchUnitTest extends AnyFlatSpec with ChiselScalatestTester {
-  val p =Parameters()
-    "FetchUnit"should "fetch two instructions and produce valid uops" in {
+  val p = Parameters()
+  "FetchUnit" should "fetch two instructions and produce valid uops" in {
     test(new FetchUnit()(p)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
 
       c.io.rob_commitsignal(0).valid.poke(false.B)
@@ -17,10 +17,9 @@ class FetchUnitTest extends AnyFlatSpec with ChiselScalatestTester {
       c.io.target_PC.poke(0.U)
       c.io.GHR.poke(0.U)
 
+
       c.io.instr(0).poke("b00000000000000000000000000010011".U) // addi x0, x0, 0
       c.io.instr(1).poke("b00000000000000000000000000010011".U)
-
-
 
 
       c.clock.step()
@@ -31,12 +30,40 @@ class FetchUnitTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  "FetchUnit" should "suppress the second uop when 2 BTB hits" in {
+  "FetchUnit" should "select correct next PC based on ROB > BP > default" in {
+    test(new FetchUnit()(p)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      c.io.rob_commitsignal(0).valid.poke(false.B)
+      c.io.rob_commitsignal(0).bits.mispred.poke(false.B)
+      c.io.rob_commitsignal(0).bits.instr_addr.poke("h1000".U)
+      c.io.id_ready.poke(true.B)
+      c.io.branch_pred.poke(true.B)
+      c.io.btb_hit(0).poke(false.B)
+      c.io.btb_hit(1).poke(false.B)
+      c.io.target_PC.poke("h3000".U)
+      c.io.GHR.poke(0.U)
+
+
+      c.io.instr(0).poke("b00000000000000000000000000010011".U)
+      c.io.instr(1).poke("b00000000000000000000000000010011".U)
+
+
+      c.clock.step()
+
+
+      c.clock.step()
+      c.io.instr_addr.expect("h3000".U)
+
+
+    }
+  }
+
+
+  "FetchUnit" should "suppress the second uop when young instr hits" in {
     test(new FetchUnit()(p)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
       c.io.rob_commitsignal(0).valid.poke(false.B)
       c.io.id_ready.poke(true.B)
       c.io.branch_pred.poke(true.B)
-      c.io.btb_hit(0).poke(true.B)
+      c.io.btb_hit(0).poke(false.B)
       c.io.btb_hit(1).poke(true.B)
       c.io.target_PC.poke(0.U)
       c.io.GHR.poke(0.U)
@@ -52,9 +79,10 @@ class FetchUnitTest extends AnyFlatSpec with ChiselScalatestTester {
       c.clock.step()
 
 
-      c.io.id_uop(0).valid.expect(true.B, "第一条应保留")
+      c.io.id_uop(0).valid.expect(true.B, "第一条应更改")
       c.io.id_uop(1).valid.expect(false.B, "第二条应舍去")
     }
   }
 }
+
 
