641b4e4261b41152f4ad935f113e14ea6a8dcd81
B83C <b83c@outlook.com>
2025-06-22 07:36:49 +0800

    more fixes

--- src/main/scala/rsd_rv32/common/freelist_cam.scala
+++ src/main/scala/rsd_rv32/common/freelist_cam.scala
@@ -33,81 +33,84 @@ class FreeListCam[T <: Data](
   val preOccupied = preOccupiedRegion.flatten.foldLeft(0.U(depth.W)){ case (mask, bit) => mask.bitSet(bit.U, true.B)}
   val masked = maskedRegions.flatten.foldLeft(0.U(depth.W)){ case (mask, bit) => mask.bitSet(bit.U, true.B)}
   // require(depth > 0 && isPow2(multiShot), "multishot length must be a power of two")
-  // val buffer = RegInit(0.U(depth.W) | preOccupied)
-  val buffer = RegInit(VecInit(preOccupied.asBools))
+  withReset(reset.asBool ||io.squeeze) {
+    
+    // val buffer = RegInit(0.U(depth.W) | preOccupied)
+    val buffer = RegInit(VecInit(preOccupied.asBools))
 
-  val should_halt_requests = if(needCheckpoint) io.restore.get else false.B
+    val should_halt_requests = if(needCheckpoint) io.restore.get else false.B
 
-  val is_full = buffer.asUInt === ~0.U(buffer.length.W) 
+    val is_full = buffer.asUInt === ~0.U(buffer.length.W) 
 
-  io.full := is_full
+    io.full := is_full
 
-  var tempMask = buffer.asUInt | masked
-  io.deq_request.map{case deq_request => {
-      val enc = PriorityEncoderOH(~tempMask(depth - 1, 0))
-      val can_deq = ~tempMask =/= 0.U
-      val idx_to_be_deq = Mux(can_deq, OHToUInt(enc), 0.U)
+    var tempMask = buffer.asUInt | masked
+    io.deq_request.map{case deq_request => {
+        val enc = PriorityEncoderOH(~tempMask(depth - 1, 0))
+        val can_deq = ~tempMask =/= 0.U
+        val idx_to_be_deq = Mux(can_deq, OHToUInt(enc), 0.U)
       
-      val can_displace = deq_request.ready || !deq_request.valid 
-      val valid = RegEnable(can_deq, 0.U, can_displace)
-      deq_request.valid := valid
-      deq_request.bits := RegEnable(idx_to_be_deq, 0.U, can_displace && can_deq) 
+        val can_displace = deq_request.ready || !deq_request.valid 
+        val valid = RegEnable(can_deq, 0.U, can_displace)
+        deq_request.valid := valid
+        deq_request.bits := RegEnable(idx_to_be_deq, 0.U, can_displace && can_deq) 
 
-      when(can_displace) {
-        buffer(idx_to_be_deq) := true.B
-      }
-      // deq_request.bits := DontCare
-      // when(deq_request.fire) {
-      //   // dbg(cf"fired enc; ${enc} idx ${idx} \n")
-      //   deq_request.bits := idx
-      //   when(!should_halt_requests) {
-      //     buffer(idx) := true.B
-      //   }
-      // }.otherwise {
-      //   deq_request.bits := DontCare
-      // }
-
-      // Skip if already valid
-      tempMask = tempMask | Mux(can_displace, enc, 0.U)
-  }}
-
-  if(directFree) {
-    val direct_free_request = io.enq_request_direct.get 
-    when(direct_free_request.valid) {
-      buffer.zip(direct_free_request.bits.asBools).map{ case (b, r) => when(r) {
-        b := false.B
-      }}
-    }
-  } else {
-     io.enq_request.foreach{case (enq) => {
-      when(!should_halt_requests) {
-        when(enq.valid) {
-          buffer(enq.bits) := false.B
+        when(!should_halt_requests && can_displace) {
+          buffer(idx_to_be_deq) := true.B
         }
+        // deq_request.bits := DontCare
+        // when(deq_request.fire) {
+        //   // dbg(cf"fired enc; ${enc} idx ${idx} \n")
+        //   deq_request.bits := idx
+        //   when(!should_halt_requests) {
+        //     buffer(idx) := true.B
+        //   }
+        // }.otherwise {
+        //   deq_request.bits := DontCare
+        // }
+
+        // Skip if already valid
+        tempMask = tempMask | Mux(can_displace, enc, 0.U)
+    }}
+
+    if(directFree) {
+      val direct_free_request = io.enq_request_direct.get 
+      when(direct_free_request.valid) {
+        buffer.zip(direct_free_request.bits.asBools).map{ case (b, r) => when(r) {
+          b := false.B
+        }}
       }
-    }   
-  }
-}
-
-  when(io.squeeze) {
-    buffer := VecInit(0.U(depth.W).asBools)
-  }
-
-  if(needCheckpoint) {
-    val restore = io.restore.get
-    val restore_mapping = io.restore_mapping.get
-    when(restore) {
-      buffer := restore_mapping
-      // printf(cf"restore_mapping : ${buffer}%b \n")
+    } else {
+       io.enq_request.foreach{case (enq) => {
+        when(!should_halt_requests) {
+          when(enq.valid) {
+            buffer(enq.bits) := false.B
+          }
+        }
+      }   
     }
   }
 
-  if(sharedState) {
-     io.state.get := buffer
-  }
+    when(io.squeeze) {
+      buffer := VecInit(0.U(depth.W).asBools)
+    }
 
-  when (true.B) {
-    val buf = buffer.asUInt
-    dbg(cf"buffer : ${buf}%b\n")
+    if(needCheckpoint) {
+      val restore = io.restore.get
+      val restore_mapping = io.restore_mapping.get
+      when(restore) {
+        buffer := restore_mapping
+        // printf(cf"restore_mapping : ${buffer}%b \n")
+      }
+    }
+
+    if(sharedState) {
+       io.state.get := buffer
+    }
+
+    when (true.B) {
+      val buf = buffer.asUInt
+      dbg(cf"buffer : ${buf}%b\n")
+    }
   }
 }
